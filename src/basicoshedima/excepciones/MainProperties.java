c58c7928f22ef9c95d73a561ed8419d6f7dde2a8
Jose <Jose@CACTUS>
2020-06-30 14:31:59 +0200

    solución humberto excepción

--- src/basicoshedima/excepciones/MainProperties.java
+++ src/basicoshedima/excepciones/MainProperties.java
@@ -22,14 +22,15 @@ import java.util.Scanner;
  *
  */
 public class MainProperties {
-	
-	/*partiendo del ejercicio de el cuento, añadid otra propiedad al fichero de 
-	 * propiedades llamada salida que contenga el nombre del fichero donde se va a escribir el cuento.
-	 *  Es decir, ya no saldará por patnall 
-	sino que el cuento será escrito en un fichero, con el nombre dado en las properties */
-	
-	public static void escribirCuentoPorPantalla (String ruta_cuento)
-	{
+
+	/*
+	 * partiendo del ejercicio de el cuento, añadid otra propiedad al fichero de
+	 * propiedades llamada salida que contenga el nombre del fichero donde se va a
+	 * escribir el cuento. Es decir, ya no saldará por patnall sino que el cuento
+	 * será escrito en un fichero, con el nombre dado en las properties
+	 */
+
+	public static void escribirCuentoPorPantalla(String ruta_cuento) {
 		Properties properties = null;
 		try {
 			properties = new Properties();
@@ -41,44 +42,59 @@ public class MainProperties {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	public static void escribirCuentoPorFichero (String ruta_cuento)
-	{
+
+	public static void escribirCuentoPorFichero(String ruta_cuento) {
 		Properties properties = null;
 		try {
 			properties = new Properties();
-			properties.load(new FileInputStream(new File(ruta_cuento)));//cargo properties
-			String nombre_fichero_salida = properties.getProperty("salida");//leo la ruta de salida
-			FileWriter cuento_salida = new FileWriter(nombre_fichero_salida);//preparo fichero salida
-			//FileWriter cuento_salida = new FileWriter(nombre_fichero_salida, true);//preparo fichero salida
+			properties.load(new FileInputStream(new File(ruta_cuento)));// cargo properties
+			String nombre_fichero_salida = properties.getProperty("salida");// leo la ruta de salida
+			FileWriter cuento_salida = new FileWriter(nombre_fichero_salida);// preparo fichero salida
+			// FileWriter cuento_salida = new FileWriter(nombre_fichero_salida,
+			// true);//preparo fichero salida
 			cuento_salida.write(properties.getProperty("start") + " " + properties.getProperty("body") + " "
-					+ properties.getProperty("end"));//escribo en él
-			cuento_salida.close();//cierro
+					+ properties.getProperty("end"));// escribo en él
+			cuento_salida.close();// cierro
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
+
+	/**
+	 * y como seria el try catch con la entrada y salida estándar? del teclado si
+	 * quieres un entero y recibes un string como controlas cuando se pide una
+	 * opción por teclado que es un numero y tu tecleas un String?
+	 */
 
 	public static void main(String[] args) {
 		String ruta_cuento = "cuento_es.properties";
-		Scanner reader = new Scanner(System.in);//scanner para leer por la entrada estándar
+		// Scanner reader = new Scanner(System.in);//scanner para leer por la entrada
+		// estándar
 		Properties properties = new Properties();
-
+		boolean entrada_numero = false;
 		int eleccion_idioma = 0;
-		
-	
-		
-		System.out.println("Elija el idioma del cuento");
-		System.out.println("Pulse 1 para Español");
-		System.out.println("Pulse 2 para Inglés");
-		System.out.println("Pulse 3 para Italiano");
+		String str_eleccion = null;
+		Scanner scanner = null;
+		do {
 
-		eleccion_idioma = reader.nextInt();
+			System.out.println("Elija el idioma del cuento");
+			System.out.println("Pulse 1 para Español");
+			System.out.println("Pulse 2 para Inglés");
+			System.out.println("Pulse 3 para Italiano");
+			try {
+				scanner = new Scanner(System.in);
+				eleccion_idioma = scanner.nextInt();
+				entrada_numero = true;
+			} catch (Exception e) {
+				System.out.println("Introduce un número, malvado");
+			} 
+		} while (!entrada_numero);
+
 		System.out.println(eleccion_idioma);
 
 		switch (eleccion_idioma) {
@@ -95,7 +111,7 @@ public class MainProperties {
 		default:
 			break;
 		}
-		//escribirCuentoPorPantalla(ruta_cuento);
+		// escribirCuentoPorPantalla(ruta_cuento);
 		escribirCuentoPorFichero(ruta_cuento);
 
 	}
--- src/basicoshedima/ficheros/MainArrayFicheros.java
+++ src/basicoshedima/ficheros/MainArrayFicheros.java
@@ -2,24 +2,39 @@ package basicoshedima.ficheros;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class MainArrayFicheros {
 
 	public static void main(String[] args) {
 
+		File file = null;
+		FileReader fileReader = null;
 		try {
 
-			File file = new File("ambrosio");
-			FileReader fileReader = new FileReader(file);
+			file = new File("ambrosio");
+			fileReader = new FileReader(file);
 			int letra_leida = 0;
+			//recorriendo el fichero
 			while ((letra_leida = fileReader.read()) != -1) {
-				System.out.println(letra_leida);
+				System.out.println(letra_leida+"-"+(char)letra_leida);
 			}
+			System.out.println("lECTURA FICHERO ACABADA!");
 
-			fileReader.close();
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
+		} finally {
+			//esta sección se va a ejecutar sí o sí
+			//haya habido una excepción o no
+			//liberar recursos
+			try {
+				fileReader.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
 		}
 	}
 
